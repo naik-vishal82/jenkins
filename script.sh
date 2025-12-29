58aa001b644e436ae5bb7b97adedbd7ec93c9a8c
Vishal Naik <93208471+naik-vishal82@users.noreply.github.com>
2025-12-29 16:30:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-docker build -t nginx-new:v01
-docker run -d -p 8080:80 --name nginx-web01 nginx-new:v01 .
+docker build -t nginx-web01:v01 .
+docker run -d -p 8080:80 --name nginx-web01 nginx-web01:v01
